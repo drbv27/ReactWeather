f281a42357def4c508a7e974e9c7563522189aee
Diego Bonilla <drbv27@gmail.com>
2022-05-13 09:09:33 -0500

    showing the data and one map

--- src/components/weatherMainInfo.js
+++ src/components/weatherMainInfo.js
@@ -7,9 +7,25 @@ const WeatherMainInfo = ({weather}) => {
         <div>{weather?.location.country}</div>
         <div>
             <div>
-                <img src={`http:${weather?.current.condition.icon}`} width="128" alt={weather?.current.condition.text}/>
+                <img 
+                src={`http:${weather?.current.condition.icon}`} 
+                width="128" 
+                alt={weather?.current.condition.text}/>
+            </div>
+            <div>
+                <div>{weather?.current.condition.text}</div>
+                <div>{weather?.current.temp_c} °C</div>
             </div>
         </div>
+        <iframe 
+        title="mapa"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d5608.705680861795!2d-75.59181796168376!3d6.26980375357124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sco!4v1652450775019!5m2!1ses!2sco"
+        width="600" height="450" 
+        style={{border:0}} 
+        allowfullscreen="" 
+        loading="lazy" 
+        referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
     </div>
   )
 }
